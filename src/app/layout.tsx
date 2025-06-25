103724053ee3a2c9883a0f9ce765168dd24ad325
zacaryn <zachary.w.head@gmail.com>
2025-06-25 15:47:07 -0400

    Update: Added google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -189,6 +189,20 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NFKGFHTJNW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NFKGFHTJNW');
+          `}
+        </Script>
+        
         <ConditionalLayout>
           {children}
         </ConditionalLayout>
